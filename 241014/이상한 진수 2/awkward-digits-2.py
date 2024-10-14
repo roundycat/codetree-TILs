b5edf82b69542b7a17d945997846bba34b37a30a
roundycat <113515003+roundycat@users.noreply.github.com>
2024-10-14 09:26:40 +0900

    Title: 이상한 진수 2, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241014/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,26 @@
+import sys
+
+INT_MIN = -sys.maxsize
+
+# 변수 선언 및 입력
+binary = list(map(int, list(input())))
+length = len(binary)
+
+# 각 i번째 자릿수를 바꾸었을 때의 십진수 값을 구해줍니다.
+ans = INT_MIN
+for i in range(length):
+	# i번째 자릿수를 바꿉니다.
+	binary[i] = 1 - binary[i]
+	
+	# 십진수로 변환합니다.
+	num = 0
+	for j in range(length):
+		num = num * 2 + binary[j]
+	
+	ans = max(ans, num)
+	
+	# i번째 자릿수를 원래대로 돌려놓습니다.
+	binary[i] = 1 - binary[i]
+
+# 출력
+print(ans)
