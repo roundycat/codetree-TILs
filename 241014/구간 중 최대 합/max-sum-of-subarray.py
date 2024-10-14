a9bd33be8cd8a5dbdb226f6684090632215b5a88
roundycat <113515003+roundycat@users.noreply.github.com>
2024-10-14 09:27:23 +0900

    Title: 구간 중 최대 합, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241014/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,16 @@
+# 변수 선언 및 입력
+n, k = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+# 모든 구간의 시작점을 잡아봅니다.
+max_sum = 0
+for i in range(0, n - k + 1):
+    # 해당 구간 내 원소의 합을 구합니다.
+    interval_sum = 0
+    for j in range(i, i + k):
+        interval_sum += arr[j]
+
+    # 최댓값을 구합니다.
+    max_sum = max(max_sum, interval_sum)
+                        
+print(max_sum)
